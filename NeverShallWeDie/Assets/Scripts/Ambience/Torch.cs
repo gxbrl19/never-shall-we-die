a10c70b961aafc4713dd31414d79bc9467ee3bf4
Gabriel Paulino <gabriel.s.paulino19@gmail.com>
2024-06-28 11:55:49 -0300

    upd torch break

--- NeverShallWeDie/Assets/Scripts/Ambience/Torch.cs
+++ NeverShallWeDie/Assets/Scripts/Ambience/Torch.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
-    private Player _player;
+    [SerializeField] AudioClip _breakSound;
+    [SerializeField] GameObject _particleHit;
+    [SerializeField] GameObject _light;
+    SpriteRenderer _sprite;
+    AudioSource _audio;
 
     private void Awake()
     {
-        _player = FindFirstObjectByType<Player>();
+        _sprite = GetComponent<SpriteRenderer>();
+        _audio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("SwordAtk"))
+        {
+            _sprite.enabled = false;
+            _light.SetActive(false);
+            _audio.PlayOneShot(_breakSound);
+            GameObject hit = Instantiate(_particleHit, transform.position, _particleHit.transform.rotation);
+            Destroy(hit, 2f);
+        }
     }
 }
