31e96d89d200bb2ab0756f194cc5c622050993cf
Gabriel Paulino <gabriel.s.paulino19@gmail.com>
2024-09-26 11:48:17 -0300

    feat: ice platform

---
+++ NeverShallWeDie/Assets/Scripts/Ambience/IcePlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcePlatform : MonoBehaviour
+{
+    float _breakDelay = 0.3f;
+    int _life = 2;
+
+    Animator _animation;
+
+    private void Awake()
+    {
+        _animation = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _life -= 1;
+            if (_life == 1) { _animation.SetBool("Collision", true); } else if (_life <= 0) { StartCoroutine(Break()); }
+        }
+    }
+
+    private IEnumerator Break()
+    {
+        yield return new WaitForSeconds(_breakDelay);
+        _animation.SetBool("Break", true);
+    }
+}
