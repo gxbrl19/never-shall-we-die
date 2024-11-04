1f21371a335dbd91b3347188fb9671457b183749
Gabriel Paulino <gabriel.s.paulino19@gmail.com>
2024-11-04 11:29:00 -0300

    feat: blacksmith npc

---
+++ NeverShallWeDie/Assets/Scripts/The Crew/Blacksmith.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blacksmith : MonoBehaviour
+{
+    public GameObject _meet;
+    public GameObject _quest;
+    public GameObject _complete;
+    public GameObject _crew;
+    public GameObject _other;
+
+
+    void Update()
+    {
+        if (_meet != null)
+        {
+            if (GameManager.instance._blacksmith == "MEET") { _meet.SetActive(true); } else { _meet.SetActive(false); }
+        }
+
+        if (_quest != null)
+        {
+            if (GameManager.instance._blacksmith == "QUEST") { _quest.SetActive(true); } else { _quest.SetActive(false); }
+        }
+
+        if (_complete != null)
+        {
+            if (GameManager.instance._blacksmith == "COMPLETE") { _complete.SetActive(true); } else { _complete.SetActive(false); }
+        }
+
+        if (_crew != null)
+        {
+            if (GameManager.instance._blacksmith == "CREW") { _crew.SetActive(true); } else { _crew.SetActive(false); }
+        }
+
+        if (_other != null)
+        {
+            if (GameManager.instance._blacksmith == "OTHER") { _other.SetActive(true); } else { _other.SetActive(false); }
+        }
+    }
+
+    public void NextState()
+    {
+        //atribui o mapa do navio
+        if (GameManager.instance._blacksmith == "OTHER") { GameManager.instance._maps[0] = 1; }
+
+        //trocando o STATE do Navegador
+        if (GameManager.instance._blacksmith == "MEET") { GameManager.instance._blacksmith = "QUEST"; }
+        else if (GameManager.instance._blacksmith == "COMPLETE") { GameManager.instance._blacksmith = "OTHER"; }
+        else if (GameManager.instance._blacksmith == "OTHER") { GameManager.instance._blacksmith = "CREW"; }
+    }
+}
