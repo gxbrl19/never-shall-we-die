23bd31c4b0926e1f4b316c7292e4c91a2c4c2681
Gabriel Paulino <gabriel.s.paulino19@gmail.com>
2025-09-10 00:40:33 -0300

    voidcaller attack sequence

--- NeverShallWeDie/Assets/Scripts/Boss/Voidcaller/Voidcaller.cs
+++ NeverShallWeDie/Assets/Scripts/Boss/Voidcaller/Voidcaller.cs
@@ -3,33 +3,53 @@ using FMODUnity;
 
 public class Voidcaller : BossBase
 {
-    private enum State { Intro, Idle, RayAttack, Octopus, SpecialAttack, Dead }
+    private enum State { Intro, Move, Idle, RayAttack, Octopus, SpecialAttack, Dead }
     private State currentState = State.Intro;
 
     [Header("Stats")]
     private bool introPlayed = false;
     [SerializeField] private float attackCooldown = 3f;
-    [SerializeField] private float specialCooldown = 15f;
     [SerializeField] private float attackTimer = 0f;
-    [SerializeField] private float specialTimer = 0f;
 
-    [Header("References")]
-    [SerializeField] private LayerMask groundLayer;
-    private Player player;
-    private Transform playerPosition;
+    [Header("Move")]
+    [SerializeField] private Transform leftLimit;
+    [SerializeField] private Transform rightLimit;
+    private float moveSpeed = 4;
     private int direction;
 
-    [Header("VoidcallerRay")]
+    [Header("References")]
+    private Player player;
+    private Transform playerPosition;
+
+    [Header("Ray")]
     [SerializeField] private Transform rayPrefab;
     [SerializeField] private Transform[] rayPoints; //pontos fixos onde os raios podem cair
+
+    [Header("Octopus")]
     [SerializeField] private GameObject octopusPrefab;
     [SerializeField] private Transform[] octopusPoints; //6ontos fixos onde o tentaculo aparecer
+
+    [Header("Sequence")]
+    int currentAttackIndex = 0;
+    State[] attackPattern = new State[]
+    {
+        State.Octopus,
+        State.Octopus,
+        State.Octopus,
+        State.RayAttack,
+        State.Octopus,
+        State.Octopus,
+        State.RayAttack,
+        State.Octopus,
+        State.RayAttack,
+    };
 
     protected override void Awake()
     {
         base.Awake();
         player = FindObjectOfType<Player>();
         playerPosition = player.GetComponent<Transform>();
+        direction = (int)transform.localScale.x;
     }
 
     void Start()
@@ -52,7 +72,6 @@ public class Voidcaller : BossBase
         if (currentState != State.Intro)
         {
             attackTimer += Time.deltaTime;
-            specialTimer += Time.deltaTime;
         }
     }
 
@@ -65,14 +84,43 @@ public class Voidcaller : BossBase
             case State.Intro:
                 break;
 
+            case State.Move:
+                animator.SetBool("Move", true);
+
+                if (direction == 1)
+                {
+                    rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+                    if (transform.position.x >= rightLimit.position.x)
+                    {
+                        direction = -1;
+                        transform.localScale = new Vector3(direction, 1, 1);
+                    }
+                }
+                else
+                {
+                    rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+
+                    if (transform.position.x <= leftLimit.position.x)
+                    {
+                        direction = 1;
+                        transform.localScale = new Vector3(direction, 1, 1);
+                    }
+                }
+
+                if (attackTimer >= attackCooldown)
+                {
+                    rb.velocity = Vector2.zero;
+                    ChangeState(State.Idle);
+                }
+                break;
+
             case State.Idle:
                 rb.velocity = Vector2.zero;
                 animator.SetBool("Move", false);
 
-                if (attackTimer >= attackCooldown && specialTimer < specialCooldown)
-                    SortAttack();
-                else if (specialTimer >= specialCooldown)
-                    ChangeState(State.RayAttack);
+                if (attackTimer >= attackCooldown)
+                    NextAttack();
                 break;
 
             case State.RayAttack:
@@ -91,31 +139,23 @@ public class Voidcaller : BossBase
                 rb.velocity = Vector2.zero;
                 animator.Play("SpecialAttack");
                 attackTimer = 0f;
-                specialTimer = 0f;
                 break;
 
             case State.Dead:
                 rb.velocity = Vector2.zero;
                 break;
         }
-
-        if (currentState == State.Idle)
-        {
-            FlipSprite();
-        }
     }
 
-    private void SortAttack()
+    private void NextAttack()
     {
-        //int attackIndex = Random.Range(0, 2);
-        int attackIndex = 0;
+        State nextState = attackPattern[currentAttackIndex];
 
-        switch (attackIndex)
-        {
-            case 0:
-                ChangeState(State.Octopus);
-                break;
-        }
+        currentAttackIndex++;
+        if (currentAttackIndex >= attackPattern.Length)
+            currentAttackIndex = 0;
+
+        ChangeState(nextState);
     }
 
     private void ChangeState(State newState)
@@ -144,18 +184,6 @@ public class Voidcaller : BossBase
         octopus.GetComponent<VoidcallerOctopus>().direction = direction;
     }
 
-    private void FlipSprite()
-    {
-        Vector2 dir = (playerPosition.position - transform.position).normalized;
-
-        if (dir.x > 0.1f)
-            direction = 1;
-        else if (dir.x < -0.1f)
-            direction = -1;
-
-        transform.localScale = new Vector3(direction, 1, 1);
-    }
-
     public void StartIntro()
     {
         if (introPlayed) return;
@@ -168,7 +196,7 @@ public class Voidcaller : BossBase
     public void StartBoss() //chamado no fim da animação de intro
     {
         ActivateBossUI();
-        ChangeState(State.Idle);
+        ChangeState(State.Move);
         player.EnabledControls();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
         //BackgroundMusic.instance.MusicControl(7);
@@ -176,17 +204,8 @@ public class Voidcaller : BossBase
 
     public void FinishAttack() //chamado ao final de qualquer ataque (menos rayattack)
     {
-        rb.velocity = Vector2.zero;
         attackTimer = 0f;
-        ChangeState(State.Idle);
-    }
-
-    public void FinishSpecialAttack() //chamado na animação RayAttack
-    {
-        rb.velocity = Vector2.zero;
-        attackTimer = 0f;
-        specialTimer = 0f;
-        ChangeState(State.Idle);
+        ChangeState(State.Move);
     }
 
     protected override void OnDeath()
